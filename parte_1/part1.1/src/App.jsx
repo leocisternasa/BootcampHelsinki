ddc084c168ac7a48e8aac58c43d0edd5eebff90e
leocisternasa <leocisal@gmail.com>
2023-12-08 14:00:02 +0100

    test commit

--- parte_1/part1.1/src/App.jsx
+++ parte_1/part1.1/src/App.jsx
@@ -39,7 +39,7 @@ const Total = ({ exercises1, exercises2, exercises3 }) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
+  const course = 'Half Stack application development PRUEBAAA'
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
   const part2 = 'Using props to pass data'
